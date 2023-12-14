46c396c8df9fc26aace408b385de30359b8a16a2
javierlnmn <jlm03123@gmail.com>
2023-12-14 21:02:28 +0100

    Routing adapted to GitHub Pages deployment

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-		<Router>
+		{/* <Router basename="react-calc/"> Swap this router tag with the other to run the build to deploy to GH Pages */}
+		<Router basename="react-calc/">
 			<Routes>
 				<Route path='/*' element={<App />} />
 			</Routes>
