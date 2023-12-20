30ce41ce379107109bee399334fa6d5bc1aa8824
javierlnmn <jlm03123@gmail.com>
2023-12-20 11:48:36 +0100

    Decimals enabled when operating solved

--- src/components/calculator/Calculator.tsx
+++ src/components/calculator/Calculator.tsx
@@ -63,6 +63,7 @@ const Calculator = (): JSX.Element => {
             setPreviousValue(result);
             setCurrentOperation(operator);
             setCurrentValue('');
+            setDecimalEnabled(false);
             return;
         }
 
